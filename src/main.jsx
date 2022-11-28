3dd78568d6fb2a8e0eb9acf9f197204eaa5256d6
ManZun93 <manuellufan19@gmail.com>
2022-11-28 01:28:11 -0500

    prueba final

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import 'bootswatch/dist/superhero/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
